1e3e42649e4eacaace328835039f1ef0c6316c02
Karol Bździuch <karlkurt1996@gmail.com>
2018-10-21 21:56:25 +0200

    drawer init

--- app/src/main/java/com/forecast/app/cryptocurrencyForcast/MainActivity.java
+++ app/src/main/java/com/forecast/app/cryptocurrencyForcast/MainActivity.java
@@ -1,77 +1,37 @@
 package com.forecast.app.cryptocurrencyForcast;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.forecast.app.api.ApiClient;
-import com.forecast.app.cryptocurrencyForcast.databinding.ActivityDashboardBinding;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import cz.msebera.android.httpclient.Header;
-
-public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    ActivityDashboardBinding dashboardBinding;
-    Cryptocurrency cryptocurrency;
-    ApiClient client;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        dashboardBinding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
 
 //        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 //        navigationView.setNavigationItemSelectedListener(this);
-        client = new ApiClient();
-        getBitcoinPLN();
-
     }
-
-    public void getBitcoinPLN() {
-        String url = "json/BTCPLN/ticker.json";
-
-        client.getCryptocurrency(url, null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                super.onSuccess(statusCode, headers, response);
-
-                try {
-                    cryptocurrency = new Cryptocurrency(response.getDouble("last"), response.getDouble("low"), response.getDouble("high"), response.getDouble("vwap"), response.getDouble("volume"));
-                    dashboardBinding.setCrypto(cryptocurrency);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                Log.i("data", String.valueOf(response));
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
-        });
-
-    }
-
 
     @Override
     public void onBackPressed() {
