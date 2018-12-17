2bb27666f2f0bbe1846362cab162cfd59bd6711e
karl5237 <karlkurt1996@gmail.com>
2018-12-17 15:22:34 +0100

    fix alarm

--- app/src/main/java/com/forecast/app/cryptocurrencyForcast/AlarmFragment.java
+++ app/src/main/java/com/forecast/app/cryptocurrencyForcast/AlarmFragment.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -17,10 +18,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.forecast.app.api.ApiClient;
+import com.forecast.app.cryptocurrencyForcast.databinding.FragmentAlarmBinding;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONException;
@@ -38,23 +41,34 @@ import static android.content.Context.MODE_PRIVATE;
  */
 public class AlarmFragment extends Fragment {
     ApiClient client;
+    FragmentAlarmBinding fragmentAlarmBinding;
+    String text;
 
     public AlarmFragment() {
         // Required empty public constructor
     }
 
-
+    String restoredText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
+//
+        fragmentAlarmBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_alarm, container, false);
+        View view = fragmentAlarmBinding.getRoot();
         getBitcoinPLN();
 
-        SharedPreferences.Editor editor = getContext().getSharedPreferences("name", MODE_PRIVATE).edit();
-        editor.putString("name", "Elena");
-        editor.putInt("idName", 12);
-        editor.apply();
+        fragmentAlarmBinding.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = fragmentAlarmBinding.bounds.getText().toString();
+                SharedPreferences.Editor editor = getContext().getSharedPreferences("name", MODE_PRIVATE).edit();
+                editor.putString("name", text);
+
+                editor.apply();
+
+            }
+        });
+
 
         Thread timer = new Thread() {
             @Override
@@ -76,11 +90,13 @@ public class AlarmFragment extends Fragment {
 
         timer.start();
 
-
+        SharedPreferences prefs = getContext().getSharedPreferences("name", MODE_PRIVATE);
+      restoredText = prefs.getString("name", null);
+        fragmentAlarmBinding.value.setText(restoredText);
 //        mainHandler.post(thread);
 
 
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+        return view;
     }
 
     public void getBitcoinPLN() {
@@ -93,7 +109,7 @@ public class AlarmFragment extends Fragment {
                 try {
 
                     double sor = Double.valueOf(response.getString("ask"));
-                    if (sor > 12150) {
+                    if (sor > Double.valueOf(restoredText)) {
                         startAlarm(true, false);
                     }
 
